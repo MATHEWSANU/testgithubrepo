c7780c53fb6b37d50385a9a15c24d5b2482dcc02
Mathews <Anu.Mathews@xylem.com>
2023-04-05 07:07:54 +0530

    Initial Commit

---
+++ Program3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpPrac;
+
+namespace CSharpPrac
+{
+    class Program3
+    {
+
+        Program p1 = new Program();
+        public void getProgram()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("-----Calling Program Method(getdata) from Program 3-----");
+            p1.getData();
+            Console.WriteLine("-----This is in Program 3 : getProgram method ending-----");
+            Console.WriteLine("");
+        }
+
+    }
+}
